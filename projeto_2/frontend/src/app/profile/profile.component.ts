a55d5acbad03fa114ada8eef5544dae341f7fb12
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-12-20 22:36:59 +0000

    Dynamic profile

--- projeto_2/frontend/src/app/profile/profile.component.ts
+++ projeto_2/frontend/src/app/profile/profile.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
+import { Component, inject } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { RouterLink } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../services/user.service';
 
 @Component({
   standalone: true,
@@ -7,9 +10,26 @@ import {CommonModule} from "@angular/common";
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
   ],
 })
-export class ProfileComponent {
-  isSuperUser = false;
+export class ProfileComponent  {
+  
+  isSuperUser: boolean = false;
+  user: User | null = null;
+  userService: UserService = inject(UserService);
+
+  constructor() {
+    let temp_user = localStorage.getItem('user');
+    if (temp_user !== null) {
+      let user_id = JSON.parse(temp_user).id;
+      this.userService.getUser(user_id).then((user) => {
+        this.user = user;
+        this.isSuperUser = (user.name == 'admin');
+      });
+    }
+  }
+
 }
+
