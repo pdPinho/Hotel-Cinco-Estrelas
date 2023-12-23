6a712c7e2db56da1cc59f3574ab371c38da65c39
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-12-23 00:00:52 +0000

    deleteUser

--- projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-users/list-users.component.ts
+++ projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-users/list-users.component.ts
@@ -15,14 +15,20 @@ import { CommonModule } from '@angular/common';
 })
 export class ListUsersComponent {
   users: User[] = [];
+  userService: UserService = inject(UserService);
 
   constructor() {
-    let userService: UserService = inject(UserService);
-    get_lists<User[]>("Users", userService).then((usersArray: User[][]) => {
+    get_lists<User[]>("Users", this.userService).then((usersArray: User[][]) => {
       // Flatten the array if necessary
       const users: User[] = usersArray.reduce((acc, curr) => acc.concat(curr), []);
       this.users = users;
       console.log(this.users);
+    });
+  }
+
+  deleteUser(user: User) {
+    this.userService.deleteUser(user).then(() => {
+      this.users = this.users.filter(u => u !== user);
     });
   }
 }
