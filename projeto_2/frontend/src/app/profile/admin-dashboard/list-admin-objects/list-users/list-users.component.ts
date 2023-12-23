7763d34db058c0d27fb44c87d8ab6c668e4344d5
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-12-23 18:06:48 +0000

    update user

--- projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-users/list-users.component.ts
+++ projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-users/list-users.component.ts
@@ -3,12 +3,14 @@ import { User } from '../../../../user';
 import get_lists from '../get-lists';
 import { UserService } from '../../../../services/user.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'list-users',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule
   ],
   templateUrl: './list-users.component.html',
   styleUrl: './list-users.component.scss'
@@ -16,6 +18,7 @@ import { CommonModule } from '@angular/common';
 export class ListUsersComponent {
   users: User[] = [];
   userService: UserService = inject(UserService);
+  selectedUser: User | null = null;
 
   constructor() {
     get_lists<User[]>("Users", this.userService).then((usersArray: User[][]) => {
@@ -26,9 +29,21 @@ export class ListUsersComponent {
     });
   }
 
+
+
   deleteUser(user: User) {
     this.userService.deleteUser(user).then(() => {
       this.users = this.users.filter(u => u !== user);
     });
+  }
+
+  updateUser(user: User) {
+    this.userService.updateUser(user).then(() => {
+      console.log("User updated");
+    });
+  }
+
+  openUserModal(person: User) {
+    this.selectedUser = person;
   }
 }
