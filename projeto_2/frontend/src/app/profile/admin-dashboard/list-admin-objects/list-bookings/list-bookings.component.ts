f75fcfcd17eef9993c7f3aa5e98c566c38eb37b5
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-12-23 20:36:19 +0000

    update bookings

--- projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-bookings/list-bookings.component.ts
+++ projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-bookings/list-bookings.component.ts
@@ -3,12 +3,17 @@ import { Booking } from '../../../../booking';
 import get_lists from '../get-lists';
 import { CommonModule } from '@angular/common';
 import { RoomsService } from '../../../../services/rooms.service';
+import { NavigationExtras, Router, RouterLink } from '@angular/router';
+import { Room } from '../../../../room';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'list-bookings',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './list-bookings.component.html',
   styleUrl: './list-bookings.component.scss'
@@ -16,8 +21,9 @@ import { RoomsService } from '../../../../services/rooms.service';
 export class ListBookingsComponent {
   bookings: Booking[] = [];
   bookingService: RoomsService = inject(RoomsService);
+  selectedBooking: Booking | null = null;
 
-  constructor() {
+  constructor(private router: Router) {
     get_lists<Booking[]>("Bookings", this.bookingService).then((bookingsArray: Booking[][]) => {
       // Flatten the array if necessary
       const bookings: Booking[] = bookingsArray.reduce((acc, curr) => acc.concat(curr), []);
@@ -30,5 +36,26 @@ export class ListBookingsComponent {
     this.bookingService.deleteBooking(booking.id!).then(() => {
         this.bookings = this.bookings.filter(u => u !== booking);
     });
-}
+  }
+
+  room_info(room: Room, isSuperUser: boolean): void {
+    const navigationExtras: NavigationExtras = {
+        state: {
+            room,
+            isSuperUser
+        }
+    };
+
+    this.router.navigate(['/admin/room'], navigationExtras);
+  }
+
+  updateBooking(booking: Booking) {
+    this.bookingService.updateBooking(booking).then(() => {
+      console.log("Booking updated");
+    });
+  }
+
+  openBookingModal(booking: Booking) {
+    this.selectedBooking = booking;
+  }
 }
