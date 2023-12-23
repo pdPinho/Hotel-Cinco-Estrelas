cd7957a72d503112275927bc2144f126a63477cb
Rafael Ferreira <rafaelcferreira@ua.pt>
2023-12-23 00:01:09 +0000

    deleteRoom

--- projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-rooms/list-rooms.component.ts
+++ projeto_2/frontend/src/app/profile/admin-dashboard/list-admin-objects/list-rooms/list-rooms.component.ts
@@ -15,14 +15,20 @@ import { CommonModule } from '@angular/common';
 })
 export class ListRoomsComponent {
   rooms: Room[] = [];
+  roomService: RoomsService = inject(RoomsService);
 
   constructor() {
-    let roomService: RoomsService = inject(RoomsService);
-    get_lists<Room[]>("Rooms", roomService).then((roomsArray: Room[][]) => {
+    get_lists<Room[]>("Rooms", this.roomService).then((roomsArray: Room[][]) => {
       // Flatten the array if necessary
       const rooms: Room[] = roomsArray.reduce((acc, curr) => acc.concat(curr), []);
       this.rooms = rooms;
       console.log(this.rooms);
+    });
+  }
+
+  deleteRoom(room: Room) {
+    this.roomService.deleteRoom(room.id).then(() => {
+      this.rooms = this.rooms.filter(u => u !== room);
     });
   }
 }
